79c68526a49a079da70c5fc86739fb4b4500be35
Robert Elias <RobertElias@users.noreply.github.com>
2020-04-24 20:21:48 -0700

    seeding database

---
+++ database/seeds/00-users.js
@@ -0,0 +1,12 @@
+exports.seed = function(knex) {
+    // Deletes ALL existing entries
+    return knex('users').del()
+        .then(function() {
+            // Inserts seed entries
+            return knex('users').insert([
+                { id: 1, username: 'MajorRobbie', password: "dinoslayer" },
+                { id: 2, username: 'MajorElias', password: "ammotek" },
+                { id: 3, username: 'CaptAmerica', password: "dinotek" }
+            ]);
+        });
+};
