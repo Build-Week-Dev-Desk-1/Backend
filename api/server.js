365c9f13ae7c870cc9afc8603ea11f1d2351f1df
Robert Elias <RobertElias@users.noreply.github.com>
2020-04-24 20:02:22 -0700

    updates

--- api/server.js
+++ api/server.js
@@ -1,3 +1,21 @@
-module.exports = {
-    jwtSecret: process.env.JWT_SECRET || "keep it secret, keep it safe!",
-};
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const authenticate = require('../auth/authenticate-middleware.js');
+const authRouter = require('../auth/auth-router.js');
+const jokesRouter = require('../userds/users-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use('/api/auth', authRouter);
+server.use('/api/users', authenticate, jokesRouter);
+server.get("/", (req, res) => {
+    res.json({ api: "This API up" });
+});
+
+module.exports = server;
