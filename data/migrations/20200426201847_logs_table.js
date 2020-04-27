d9d5d3b4f0d4fa7d4751ab0d1af3d8fb73df9035
Robert Elias <RobertElias@users.noreply.github.com>
2020-04-27 10:54:01 -0700

    updates

--- data/migrations/20200426201847_logs_table.js
+++ data/migrations/20200426201847_logs_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('logs', logs => {
 
         logs.increments();
-        //logs.integer("userid").notNullable().references("id").inTable("users");
+
         logs.string("title", 255).notNullable();
         logs.string("description", 800).notNullable();
         logs.boolean("completed").defaultTo(false);
