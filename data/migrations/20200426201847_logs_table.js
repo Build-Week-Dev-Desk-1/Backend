6054bb170863548184e8e230032a3c06c332d516
Robert Elias <RobertElias@users.noreply.github.com>
2020-04-27 11:40:48 -0700

    updates

--- data/migrations/20200426201847_logs_table.js
+++ data/migrations/20200426201847_logs_table.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('logs', logs => {
 
         logs.increments();
-
+        logs.integer('userid').unsigned().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
         logs.string("title", 255).notNullable();
         logs.string("description", 800).notNullable();
         logs.boolean("completed").defaultTo(false);
@@ -10,7 +10,7 @@ exports.up = function(knex) {
         // define a Foreign Key
         // foreign key to user table // integer without negative values, db uses sign bit for larger #s
         // regards deleting recrod from the primary key table // onDelete() can take 'RESTRICT', 'NO ACTION', 'SET NULL', 'CASCADE'
-        logs.integer('userid').unsigned().references('id').inTable('users').onDelete('CASCADE').onUpdate('CASCADE');
+
         // regards chaging the value of the primary key table
     })
 };
