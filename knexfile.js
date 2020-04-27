76629e5b9cfcd152cef844cf8662c2e541bea5bb
Robert Elias <RobertElias@users.noreply.github.com>
2020-04-26 23:35:17 -0700

    updates to knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,6 @@ module.exports = {
         useNullAsDefault: true,
         migrations: {
             directory: './data/migrations',
-            tableName: 'dbmigrations',
         },
         seeds: { directory: './data/seeds' },
     },
@@ -25,35 +24,16 @@ module.exports = {
             directory: "./data/seeds",
         },
     },
-    staging: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
-        migrations: {
-            tableName: 'knex_migrations'
-        }
-    },
 
+    //Heroku will look for production configuration
     production: {
-        client: 'postgresql',
-        connection: {
-            database: 'my_db',
-            user: 'username',
-            password: 'password'
-        },
-        pool: {
-            min: 2,
-            max: 10
-        },
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
         migrations: {
-            tableName: 'knex_migrations'
-        }
-    }
+            directory: "./data/migrations",
+        },
+        seeds: {
+            directory: "./data/seeds",
+        },
+    },
 };
