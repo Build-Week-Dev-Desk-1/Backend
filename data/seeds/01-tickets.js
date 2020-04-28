799af85ae12459cf7dd32f8bfecd8acd97b427bd
Robert Elias <RobertElias@users.noreply.github.com>
2020-04-28 15:44:23 -0700

    updates

--- data/seeds/01-tickets.js
+++ data/seeds/01-tickets.js
@@ -4,9 +4,9 @@ exports.seed = function(knex) {
         .then(function() {
             // Inserts seed entries
             return knex('tickets').insert([
-                { userid: 1, title: 'My mac does not work', description: 'You will figure out what to put when you try it...', completed: "false" },
-                { userid: 2, title: 'React.js is having issues', description: 'You will figure out when you see this data set...', completed: "false" },
-                { userid: 3, title: 'The slack migration was not working', description: 'You will figure out when you see this data set...', completed: "false" }
+                { userid: 1, id: 1, title: 'My mac does not work', description: 'You will figure out what to put when you try it...', completed: "false" },
+                { userid: 2, id: 2, title: 'React.js is having issues', description: 'You will figure out when you see this data set...', completed: "false" },
+                { userid: 3, id: 3, title: 'The slack migration was not working', description: 'You will figure out when you see this data set...', completed: "false" }
             ]);
         });
 };
